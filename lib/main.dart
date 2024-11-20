f79a532083186bf1f0db20f8d488b1298f4ccafc
Sanjarbek17 <sanjarsaidov173@gmail.com>
2024-11-20 16:15:28 +0500

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: lightTheme,
       onGenerateRoute: CustomRouter.generateRoute,
-      initialRoute: CustomRouter.appView,
+      initialRoute: CustomRouter.splashPage,
     );
   }
 }
